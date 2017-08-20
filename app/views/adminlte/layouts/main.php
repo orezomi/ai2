30e58cdb6618aa13a12af0fa2b58532da31dd729
aji <harismo@ji.com>
2017-08-20 16:59:41 +0700

    image dimension meta data

---
+++ app/views/adminlte/layouts/main.php
@@ -0,0 +1,90 @@
+<?php
+use yii\helpers\Html;
+use yii\bootstrap\Modal;
+use app\modules\admin\models\Settings;
+
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+$setting = Settings::find()->where(['active'=>true])->one();
+$config = json_decode($setting->config,true);
+if (Yii::$app->controller->action->id === 'login') { 
+/**
+ * Do not use this code in your template. Remove it. 
+ * Instead, use the code  $this->layout = '//main-login'; in your controller.
+ */
+    echo $this->render(
+        'main-login',
+        ['content' => $content]
+    );
+} else {
+
+    if (class_exists('backend\assets\AppAsset')) {
+        backend\assets\AppAsset::register($this);
+    } else {
+        app\assets\AppAsset::register($this);
+    }
+
+    dmstr\web\AdminLteAsset::register($this);
+
+    $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
+    ?>
+    <?php $this->beginPage() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>">
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+    </head>
+    <body class="hold-transition <?=$config['theme']?> sidebar-mini sidebar-collapse">
+    <?php $this->beginBody() ?>
+    <div class="wrapper">
+
+        <?= $this->render(
+            'header.php',
+            ['directoryAsset' => $directoryAsset]
+        ) ?>
+
+        <?= $this->render(
+            'left.php',
+            ['directoryAsset' => $directoryAsset]
+        )
+        ?>
+
+        <?= $this->render(
+            'content.php',
+            ['content' => $content, 'directoryAsset' => $directoryAsset]
+        ) ?>
+
+    </div>
+
+    <?php 
+    Modal::begin([
+            'header'=>'<h4>Data</h4>',
+            'id'=>'modal',
+            'size'=>'modal-lg',
+        ]);
+        echo "<div id='modalContent'></div>";
+        Modal::end();
+    ?>
+
+    <!-- <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-104753715-1', 'auto');
+      ga('send', 'pageview');
+
+    </script> -->
+
+    <?php $this->endBody() ?>
+    </body>
+    </html>
+    <?php $this->endPage() ?>
+<?php } ?>
---
+++ app/views/adminlte/layouts/plain3.php
@@ -0,0 +1,54 @@
+<?php
+use app\assets\Layerslider;
+use yii\helpers\Html;
+use app\modules\admin\models\Settings;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+dmstr\web\AdminLteAsset::register($this);
+app\assets\AppAsset::register($this);
+
+$directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
+$setting = Settings::find()->where(['active'=>true])->one();
+$config = json_decode($setting->config,true);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+	<div class="hold-transition <?=$config['theme']?> sidebar-collapse">
+		<?= $this->render(
+	        'header1.php',
+	        ['directoryAsset' => $directoryAsset]
+	    ) ?>
+	    <?= $this->render(
+	        'content3.php',
+	        [
+	        	'directoryAsset' => $directoryAsset,
+	        	'content' => $content 
+	        ]
+	    ) ?>
+	</div>
+	<!-- <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-104753715-1', 'auto');
+      ga('send', 'pageview');
+
+    </script> -->
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
